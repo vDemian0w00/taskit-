4513f17cdb0d1ae1b357f25c5d6e04b1fe02619b
vDemian0w00 <cfajardoa2000@alumno.ipn.mx>
2023-02-25 11:01:01 -0600

    preparing for merging main-cer into dev-demian

--- client/src/navigation/index.tsx
+++ client/src/navigation/index.tsx
@@ -1,3 +1,25 @@
+import { memo } from "react";
 import MainNavigator from "./MainNavigator";
 
-export default MainNavigator;
+import {
+  ConnectionProvider,
+  WalletProvider,
+} from "@solana/wallet-adapter-react";
+import { PhantomWalletAdapter } from "@solana/wallet-adapter-wallets";
+import { clusterApiUrl } from "@solana/web3.js";
+import { useMemo } from "react";
+const Index = memo(() => {
+  const wallets = useMemo(() => [new PhantomWalletAdapter()], []);
+
+  const endpoint = useMemo(() => clusterApiUrl("devnet"), []);
+
+  return (
+    <ConnectionProvider endpoint={endpoint}>
+      <WalletProvider wallets={wallets} autoConnect>
+        <MainNavigator />
+      </WalletProvider>
+    </ConnectionProvider>
+  );
+});
+
+export default Index;
