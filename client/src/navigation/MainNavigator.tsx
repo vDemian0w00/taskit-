69d8eccacd45320808ded1bddc5b7784447675b3
Elgussgood <gsuarezr2000@alumno.ipn.mx>
2023-02-25 09:58:42 -0600

    FRONT: Base to Manu

--- client/src/navigation/MainNavigator.tsx
+++ client/src/navigation/MainNavigator.tsx
@@ -2,12 +2,13 @@ import Index from "@scenes/index";
 import { ROUTES } from "@utils/ROUTES";
 import { memo } from "react";
 import { Routes, Route } from "react-router-dom";
+import { Main } from "@scenes/main/main";
 
 const MainNavigator = memo(() => {
   return (
     <Routes>
       <Route index path={ROUTES.INDEX} element={<Index />} />
-      <Route path={ROUTES.MAIN} element={<div>frecuence</div>} />
+      <Route path={ROUTES.MAIN} element={<Main/>} />
       <Route path={ROUTES.SELECT_PROFILE} element={<div>analytics</div>} />
       {/* <Route path={ROUTES.Config} element={<div>config</div>} /> */}
 
