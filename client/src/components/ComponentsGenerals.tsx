3b488dc324967d9fd894a2913d4da75e30d61f49
Sof1a-m3n2 <111402138+SMenaF@users.noreply.github.com>
2023-02-25 18:03:31 -0600

    FRONT: Principal page // 60%

--- client/src/components/ComponentsGenerals.tsx
+++ client/src/components/ComponentsGenerals.tsx
@@ -1,11 +1,14 @@
 import React from "react";
-import "./ComponentsGenerals.css";
+import "./ComponentsGenerals.css";  
+import Image from "@assets/logo_taskit.png";
+import '@components/ComponentsGenerals.css'
 
 export const Navbar = () => {
   return (
     <div className='containerNavbar'>
         <div className='squartNavbar'>
             <div className='logoNavbar'>
+              
             </div>
             <div className='search'></div>
             <div className='profile'></div>
--- client/src/scenes/index/index.tsx
+++ client/src/scenes/index/index.tsx
@@ -1,17 +1,47 @@
 import React from 'react'
-import { Navbar } from '@components/ComponentsGenerals'
 import './index.css'
+import Logo from '@assets/logo_taskit.png'
 
 type Props={}
 
 const Index = (props:Props) => {
   return (
     <>
-      <div className="conteinerIndex">
-        <Navbar />
-        <div className='index'>
-          hola
-        </div>
+      <div className="conteinerIndex">    
+
+          <header className="header">
+
+            <div className="logo">
+              <img src={Logo} alt="Logo" className='image' />
+            </div>
+
+            <div className="otherEle"><a href=""></a></div>
+            <div className='loginButton'><a href=""></a></div>
+          </header>
+
+          <div className="conteinerBoth">
+            <div className='left'>
+
+              <div className='contLeft'>
+
+                <div className='centerLeft1'>
+                  Do you have an idiom task? Get it fast!
+                </div>
+
+                <div className='centerLeft2'>
+                  Task It it's a platform that allows you to get your idiom task done in a fast and easy way. 
+                  You can help others with their tasks and get paid for it.
+                </div>
+
+              </div>
+
+            </div>
+  
+            <div className='right'>
+  
+            </div>
+          </div>
+
       </div>
     </>
   )
