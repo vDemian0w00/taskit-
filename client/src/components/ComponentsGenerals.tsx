592bb9f56e530ed10a0b8f7de08217763b7561b7
Elgussgood <gsuarezr2000@alumno.ipn.mx>
2023-02-26 05:02:42 -0600

    FRONT: Navbar // 100%

--- client/src/components/ComponentsGenerals.tsx
+++ client/src/components/ComponentsGenerals.tsx
@@ -4,9 +4,9 @@ import "./ComponentsGenerals.css";
 export const Navbar = () => {
   return (
     <div className='Navbar'>
-      <div className="NavBarIn">
+      <div className="NavbarIn">
         <div className='LogoNavbar'></div>
-        <div className='SearchingNavbar'></div>
+        <div className='SearchNavbar'></div>
         <div className='ProfileNavbar'></div>
       </div>
     </div>
