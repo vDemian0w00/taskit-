c37f187b7c1bc910271bf7a3c94dd3437380fb02
Jesus Manuel Paredon Rivera <jmparedonr2005@gmail.com>
2023-02-27 10:10:32 -0600

    FRONT: Tasks views

--- client/src/scenes/task/pendingTask.tsx
+++ client/src/scenes/task/pendingTask.tsx
@@ -1,12 +1,19 @@
 import React from 'react'
+import {Task} from '@scenes/task/componentsTask'
 
 type Props={}
 
 const pendingTask = (props:Props) => {
     return (
-        <div>
-            
-        </div>
+        <>
+            <Task viewTitle={'Tarea pendiente que hará el Experto'} 
+            taskName={'Tarea English'} 
+            usExpertFlag={false} 
+            exPostFlag={false} 
+            exAcceptFlag={false} 
+            usAcceptFlag={true} 
+            info={''} />
+        </>
     )
 }
 
