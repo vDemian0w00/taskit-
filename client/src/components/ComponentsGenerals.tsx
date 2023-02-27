0c2bf80efd3851d06db9184b75585b9c23c6671d
vDemian0w00 <cfajardoa2000@alumno.ipn.mx>
2023-02-27 10:52:03 -0600

    WalletMulti

--- client/src/components/ComponentsGenerals.tsx
+++ client/src/components/ComponentsGenerals.tsx
@@ -4,31 +4,39 @@ import "@components/ComponentsGenerals.css";
 import Logo from "@assets/LogoColors.png";
 import { Link } from "react-router-dom";
 import Photo from "@assets/User.png";
+import { WalletMultiButton } from "@solana/wallet-adapter-react-ui";
 type PropsUser = {
-  nameusu:string
-}
-export const Navbar = ({nameusu}:PropsUser) => {
+  nameusu: string;
+};
+export const Navbar = ({ nameusu }: PropsUser) => {
   return (
     <div className="Navbar">
-        <div className="ProfileNavbar">
-          <div className="PhotoUser">
+      <div className="ProfileNavbar">
+        <div className="PhotoUser">
           <Link to={"/"}>
             <div className="CirUser">
-               <img src={Photo} className='photousu'/> 
+              <img src={Photo} className="photousu" />
             </div>
           </Link>
-          </div>
-          <div className="NameUser">
-            <h2>{nameusu}</h2>
-          </div>
         </div>
-        <div className="SearchNavbar">
-          <div className="navicon"><i className="fa-solid fa-magnifying-glass"></i></div>
-          <div className="navinput"><input type="text" /></div>
+        <div className="NameUser">
+          <h2>{nameusu}</h2>
         </div>
-        <div className="LogoNavbar">
-            <img src={Logo} className='Logo'/> 
-        </div>
+      </div>
+      <div className="SearchNavbar">
+        {/* <div className="navicon"><i className="fa-solid fa-magnifying-glass"></i></div>
+          <div className="navinput"><input type="text" /></div> */}
+        <WalletMultiButton
+          style={{
+            height: "8vh",
+            width: "20vw",
+            margin: 0,
+          }}
+        />
+      </div>
+      <div className="LogoNavbar">
+        <img src={Logo} className="Logo" />
+      </div>
     </div>
   );
 };
